e3023757db9c4e8a4c2f384b1c022b9ea671787d
Paul <paul_heinemeyer@outlook.de>
2022-01-04 02:50:41 +0100

    inserted static bernoulli functions from original implementation

--- Anchor/candidate.py
+++ Anchor/candidate.py
@@ -9,4 +9,8 @@ class AnchorCandidate:
     Reprensents a possible candidate in the process of finding the best anchor.
     """
 
-    ...
+    precision: float = 0
+
+    @property
+    def precision(self):
+        return self.precision
--- Anchor/kl-lucb.py
+++ Anchor/kl-lucb.py
@@ -12,6 +12,9 @@ class KL_LUCB:
     """
     Multi armed bandit with lower and upper bound.
     Used to find the anchor rule with the expected highest precision.
+
+    More information can be found in the following paper:
+    http://proceedings.mlr.press/v30/Kaufmann13.pdf
     """
 
     eps: float = field()
@@ -21,37 +24,70 @@ class KL_LUCB:
 
     # TODO: fix type annotations and implement this shit
     def get_best_candidates(
-        self, canditates: list[AnchorCandidate], sampler, top_n: int = 1,
+        self, candidates: list[AnchorCandidate], sampler, top_n: int = 1,
     ):
-        prec_ub = torch.zeros(len(canditates))
-        prec_lb = torch.zeros(len(canditates))
+        """
+        Find top-n anchor candidates with highest expected precision.
+        """
+        t = 1
+        prec_ub = torch.zeros(len(candidates))
+        prec_lb = torch.zeros(len(candidates))
+
+        lt, ut = self.__update_bounds(candidates, prec_lb, prec_ub, t)
 
         while (prec_ub - prec_lb) > self.delta:
             pass
 
+        # @TODO Decide what should be returned. Could be the top n indices or the means of all candidates.
+
     def __update_bounds(
-        canditates: list[AnchorCandidate], ub: list[float], lb: list[float], t: int
+        candidates: list[AnchorCandidate], lb: list[float], ub: list[float], t: int
     ) -> Tuple[list[float], list[float]]:
         """
         Updates current bounds
         """
-        ...
+        means = [c.precision for c in candidates]  # mean precision per candidate
+        # @TODO Implement
+
+    # Following part is completely based on the original implementation, since there is not much one could optimize or change
 
     @staticmethod
     def compute_beta(n_features: int, t: int, delta: float):
-        ...
+        alpha = 1.1  # constant from paper
+        k = 405.5  # constant from paper
+        temp = np.log(k * n_features * (t ** alpha) / delta)
+
+        return temp + np.log(temp)
 
     @staticmethod
     def dup_bernoulli(precision: float, level: float):
-        ...
+        lm = precision
+        um = min(min(1, precision + np.sqrt(level / 2.0)), 1)
+        qm = (um + lm) / 2.0
+
+        if KL_LUCB.kl_bernoulli(precision, qm) > level:
+            um = qm
+        # dont know why this should make sense at all?
+        # else:
+        #     lm = qm
+        return um
 
     @staticmethod
     def dlow_bernoulli(precision: float, level: float):
-        ...
+        um = precision
+        lm = max(min(1, precision - np.sqrt(level / 2.0)), 0)
+        qm = (um + lm) / 2.0
+
+        if KL_LUCB.kl_bernoulli(precision, qm) > level:
+            lm = qm
+        # else:
+        #     um = qm
+        return lm
 
     @staticmethod
     def kl_bernoulli(precision: float, q: float):
         p = min(0.9999999999999999, max(0.0000001, precision))
         q = min(0.9999999999999999, max(0.0000001, q))
+
         return p * np.log(float(p) / q) + (1 - p) * np.log(float(1 - p) / (1 - q))
 
